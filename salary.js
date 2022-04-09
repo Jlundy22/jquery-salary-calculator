6a7208f06859f4b584713d259b8f84e4de16aa85
Jack Lund <jacklund920@gmail.com>
2022-04-09 13:33:53 -0500

    inputs now append to the DOM and input feilds are emptied

--- salary.js
+++ salary.js
@@ -9,21 +9,25 @@ function readyNow() {
 
 function submitButtonClicked() {
     const firstNameInput = $('#firstNameInput').val();
-    const lastNameInput = $('#lastNameInput').val()
-    const idInput = $('#idInput').val()
-    const titleInput = $('#titleInput').val()
-    const salaryInput = $('#annualSalaryInput').val()
-    console.log(firstNameInput,lastNameInput,idInput,titleInput,salaryInput)
+    const lastNameInput = $('#lastNameInput').val();
+    const idInput = $('#idInput').val();
+    const titleInput = $('#titleInput').val();
+    const salaryInput = $('#annualSalaryInput').val();
+    $('#employeeInfo').append(`
+    <tr>
+    <td>${firstNameInput}</td>
+    <td>${lastNameInput}</td>
+    <td>${idInput}</td>
+    <td>${titleInput}</td>
+    <td>${salaryInput}</td>
+    </tr>`);
+      $('#firstNameInput').val('');
+      $('#lastNameInput').val('');
+      $('#idInput').val('');
+      $('#titleInput').val('');
+      $('#annualSalaryInput').val('');
     
 }
 
 
 
-// $('#employeeInfo').append(`
-//     <tr>
-//     <td>1Hello</td>
-//     <td>2Hello</td>
-//     <td>3Hello000000000000000000</td>
-//     <td>4Hellooooooooooooo</td>
-//     <td>5Hello</td>
-//     </tr>`)
