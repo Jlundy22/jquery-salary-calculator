ba82905599ce02a7b32890d00ddbc00d31e27bd4
Jack Lund <jacklund920@gmail.com>
2022-04-09 13:22:48 -0500

    Basic html outline setup,submit button works to grab the info from all inputs

--- salary.js
+++ salary.js
@@ -4,4 +4,26 @@ $(document).ready(readyNow);
 
 function readyNow() {
     console.log('jq');
+    $('#submitButton').on('click',submitButtonClicked)
 }
+
+function submitButtonClicked() {
+    const firstNameInput = $('#firstNameInput').val();
+    const lastNameInput = $('#lastNameInput').val()
+    const idInput = $('#idInput').val()
+    const titleInput = $('#titleInput').val()
+    const salaryInput = $('#annualSalaryInput').val()
+    console.log(firstNameInput,lastNameInput,idInput,titleInput,salaryInput)
+    
+}
+
+
+
+// $('#employeeInfo').append(`
+//     <tr>
+//     <td>1Hello</td>
+//     <td>2Hello</td>
+//     <td>3Hello000000000000000000</td>
+//     <td>4Hellooooooooooooo</td>
+//     <td>5Hello</td>
+//     </tr>`)
